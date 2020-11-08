a5cd5ba1187bb6fcc20d73a546a8697dbda634d8
avavadhoot0204 <aviash024@gmail.com>
2020-11-07 19:08:03 -0500

    Updates

---
+++ precache-manifest.c5ac9424b0aaa024edd513fc7d8ea9c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7ad61b50672d0710504",
+    "url": "/Portfolio/static/css/main.2aa1955b.chunk.css"
+  },
+  {
+    "revision": "f7ad61b50672d0710504",
+    "url": "/Portfolio/static/js/main.f7ad61b5.chunk.js"
+  },
+  {
+    "revision": "21569f65dffe88595d8b",
+    "url": "/Portfolio/static/js/1.21569f65.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "bbe77924efb4172bdd81aaaeade625a4",
+    "url": "/Portfolio/index.html"
+  }
+];
