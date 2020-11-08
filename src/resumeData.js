18be1bfe2540bcbb1bca2cae580a8e72beb9f583
avavadhoot0204 <aviash024@gmail.com>
2020-11-07 19:06:59 -0500

    deploying portfolio to github pages

---
+++ src/resumeData.js
@@ -0,0 +1,147 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/avavadhoot0204",
+    "name": "Avadhoot Pawaskar",
+    "role": "Data Analyst | Data Scientist | Business Analyst",
+    "linkedinId":"https://www.linkedin.com/in/avadhootpawaskar/",
+    "phoneNumber":"862-237-5216",
+    "emailid": "ap1731@scarletmail.rutgers.edu",
+    "gemail": "pawaskaravadhoot04@gmail.com",
+    "address": "Harrison, New Jersey",
+    "roleDescription": "Hi! I am you next Data Analyst / Data Scientist. Exceptionally innovative Data Analyst with a proven record of database design and project management. Highly analytical and detail-oriented data analyst with in-depth knowledge of database types; research methodologies; and significant data capture, curation, manipulation, and visualization. Furnish insights, analytics, and business intelligence used to advance opportunity identification, process reengineering, and corporate growth.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/avadhootpawaskar/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/avavadhoot0204",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"https://www.instagram.com/iamavadhootpawaskar/",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"Exceptionally innovative Data Analyst with a proven record of database design and project management. Highly analytical and detail-oriented data analyst with in-depth knowledge of database types; research methodologies; and significant data capture, curation, manipulation, and visualization. Furnish insights, analytics, and business intelligence used to advance opportunity identification, process reengineering, and corporate growth. Possessing great problem solving and analytical abilities, I would like to pursue my career as a Data Analyst. Currently pursuing a Masters degree in Information Technology and Analytics with Specialization in Data Analytics from Rutgers University, Newark NJ. I would like to work in a challenging environment where I would get an opportunity to test and polish my skills becoming an expert in the area of data science field",
+    "address":"Harrison, NJ",
+    "website":"https://github.com/avavadhoot0204",
+    "education":[
+      {
+        "UniversityName":"Rutgers University",
+        "specialization":"Master of Science in Information Technology and Analytics",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2020",
+        "Gpa":"3.6",
+        "Achievements":"Relevant Courses: Data Analysis and Visualization, Machine Learning and Statistics, Business Data Management, Analytics Business Intelligence, Project Management",
+        "AboutEdu": "My Graduate coursework has strengthen my concepts of Product Development, Systems Design, Object oriented princeples, Database Management and Database Design. I have gained knowledge and experience in web/application development throughout the software development lifecycle. Also, I have been following agile development methodology. I am always open to learning and so the technology stack I have selected for my graduation is diverse, ranging from frontend development to backend development and also Data Analysis and Data Science related courses. Apart from this, I have developed strong communication skills throughtout my project presentations and working in group improving my teamwork ability further. My coursework has helped me enhance my problem solving and analytical abilities."
+      },
+      {
+        "UniversityName":"University of Mumbai",
+        "specialization":"Bachelors in Computer Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017",
+        "Gpa":"3.1",
+        "Achievements":"Relevant courses: Object-Oriented Programming Methodology, Data Structures, Web Programming, Structured Programming Approach, Artificial Intelligence, Database Management Systems, Advanced Database Management Systems",
+        "AboutEdu": "My Bachelor's coursework was a varied technological stack along with courses related to soft-skills. This coursework helped me in all-round development to become a successful software engineer. I develped a capstone project using AndroidSDK increasing my proficiency in Java and SQL. Also these courses helped me during my work-experience right from technical skills to soft skills, as my company was able to gain success in proposing development of new application, through my client communication and in-depth technical knowledge"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"HERE Solutions India Private Limited",
+        "specialization":"Database Analyst",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2019",
+        "Achievements":"Automated database backup, restoration, report generation, performed the analysis with ETL tools increasing efficiency by 31%, Constructed pattern matching code snippets for 10,000+ roads to automatically instruct cars about navigation, Created and executed 50+ SQL nested queries daily, constructed KPI dashboard for data analysis with excel macros, pivot tables, Conducted data cleaning, preprocessing to remove inconsistency in CSV files before consumption by web services, Performed market analysis to efficiently using Tableau dashboards to achieve objectives, increasing sales by 24%, Received appreciation for automation of report generation, saving 2 hours of time and combined efforts on repetitive tasks daily"
+      },
+      {
+        "CompanyName":"Four Brothers Software Solutions",
+        "specialization":"Data Analyst Intern",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2015",
+        "Achievements":"Optimized pricing by capturing, cleansing, manipulating, and conducting visualization of raw data to assist in analytical data services using modern Business Intelligence and Data Visualization tools such as Tableau along with increasing efficiency by 28%, Estimated customer purchase behavior by designing and building models on large datasets and automating the analytics process, Utilized Google Analytics and Google Tag Manager and implemented new scripts that increased performance by 25%, Incorporated smooth automation into repetitive business tasks and lengthy processes using Python"
+      }
+    ],
+    "skills":[
+      {
+        "skillname":"SQL",
+      },
+      {
+        "skillname":"Python",
+      },
+      {
+        "skillname":"Tableau",
+      },
+      {
+        "skillname":"Statistics",
+      },
+      {
+        "skillname":"MSOffice",
+      },
+      {
+        "skillname":"AWS",
+      },
+      {
+        "skillname":"HTML5",
+      },
+      {
+        "skillname":"React",
+      },
+      {
+        "skillname":"Javascript",
+      },
+      {
+        "skillname":"Java",  
+      },
+      {
+        "skillname":"GoogleAnalytics",
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Suicide Rate Analysis",
+        "description":"application",
+        "imgurl":"images/portfolio/SuicideRate.jpg",
+        "url":"https://github.com/avavadhoot0204/Suicide-Rate-Analysis"
+      },
+      {
+        "name":"Movie Recommendation System",
+        "description":"application",
+        "imgurl":"images/portfolio/MovieRecommendation.jpg",
+        "url":"https://github.com/avavadhoot0204/Movie-Recommendation"
+      },
+      {
+        "name":"Telecommunication Management System",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/teleManagement.jpg",
+        "url":"https://github.com/avavadhoot0204/Telecommunication-Server-Management-System"
+      },
+      {
+        "name":"Song Recommender System",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/songImage.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Avadhoot is a great team player with good communication skill. he is highly motivated and passionate towards his career, always willing to learn new skills and technologies. Always focused towards work and target to complete within deadlines. Avadhoot has my highest recommendation.",
+        "name":"Sriram Naraynan, Project Manager at HERE Solutions India Private Limited"
+      },
+      {
+        "description":"Avadhoot Pawaskar is a hard working individual who makes sure the project timeline is followed and motivates other team members to give their best. He is very sharp and very good at analyzing and giving a right solution at its best. He is an asset to the company by his nature of work and solutions and commitment. I am sure with his technical finesse and contagious attitude he will reach great heights in his career. Apart from his technical skills he is very good as a person. I wish him all the best for his future career goals",
+        "name":"Jayashree Gokhale, Project Manager at Rave Technologies"
+      },
+      {
+        "description":"I had the privilege of working with Avadhoot and he is proactive, result oriented, responsible and technically sound employee and always ready to put all his energy and time to get the job done. He has an exceptional troubleshooting and analytical skill in SQL & C# technologies. He is a great asset to any company.",
+        "name":"Jugal Nathani, Data Engineer at HERE Solutions India Private Limited"
+      },
+      {
+        "description":"Avadhoot has been one of the most hardworking and dedicated subordinates I have come across. Being his trainer and reporting manager for some of the assignments, I can confidently vouch for his quick learning abilities and his customer handling skills. Within few months of working on the assignments, he was counted as one of trusted resources when it came to project deliveries. I was always amazed to see how he balanced his extra curricular activities within organization along with project pressures. With his management skills, dedication and effort, he surely will go way ahead in his career.",
+        "name":"Kunal Chavan, Spatial Data Specialist I at HERE Solutions India Private Limited"
+      }
+    ]
+  }
+  
+  export default resumeData
